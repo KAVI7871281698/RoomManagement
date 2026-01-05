33e6c043b3a10f9b1feefd9d76a402185c8b010d
KAVI7871281698 <kavipriyan292004@gmail.com>
2026-01-05 22:01:57 +0530

    changes

---
+++ lib/app_image.dart
@@ -0,0 +1,6 @@
+class AppIcon {
+  static const String home = 'assets/icons/home.png';
+  static const String wallet = 'assets/icons/wallet.png';
+  static const String members = 'assets/icons/group.png';
+  // static const String settings = 'assets/icons/settings.png';
+}
